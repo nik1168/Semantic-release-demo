3664c2ee5ff7f09829c4b8275a2c74ab8db4b42c
ngeisser <ngeisser@tetradefense.com>
2021-08-27 11:28:44 -0400

    feat: add plugins

--- release.config.js
+++ release.config.js
@@ -4,6 +4,9 @@ module.exports = {
         "@semantic-release/release-notes-generator",
         "@semantic-release/changelog",
         "@semantic-release/npm",
-        "@semantic-release/git"
+        [
+            "@semantic-release/git",
+            {"assets": ["CHANGELOG.md"]}
+        ]
     ]
 }
